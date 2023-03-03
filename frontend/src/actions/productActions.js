8b833010d299fa402ba2fcae4cae6088a08332f5
TrinhDucLoc <westudymore@gmail.com>
2023-03-03 11:03:05 +0700

    Product page - productDetailAction, productDetailReducer, productDetailConstants

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,6 +3,9 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAIL_REQUEST,
+  PRODUCT_DETAIL_SUCCESS,
+  PRODUCT_DETAIL_FAIL,
 } from "../constants/productConstants";
 
 export const listProducts = () => async (dispatch) => {
@@ -19,6 +22,30 @@ export const listProducts = () => async (dispatch) => {
     dispatch({
       type: PRODUCT_LIST_FAIL,
       payload: error.message,
+    });
+  }
+};
+
+export const detailsProduct = (productId) => async (dispatch) => {
+  dispatch({
+    type: PRODUCT_DETAIL_REQUEST,
+    payload: productId,
+  });
+  try {
+    // REST API product detail
+    const { data } = await Axios.get("/api/product/${productId}");
+    // dispatch data response
+    dispatch({
+      type: PRODUCT_DETAIL_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAIL_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
     });
   }
 };
