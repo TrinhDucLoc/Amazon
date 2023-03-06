df09b06016f3a481983190ef9e6b1cd5d8e48282
TrinhDucLoc <westudymore@gmail.com>
2023-03-06 14:52:20 +0700

    Cart Page - Save cartItems into localStorage and display length of cartItems

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,24 @@
+import { CART_ADD_ITEM } from "../constants/cartConstants";
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      const item = action.payload;
+      const existItem = state.cartItems.find(
+        (x) => x.productId === item.productId
+      );
+      //   If exist item => save update value of item. Else add item in the end cartItems
+      if (existItem) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((x) =>
+            x.productId === existItem.productId ? item : x
+          ),
+        };
+      } else {
+        return { ...state, cartItems: [...state.cartItems, item] };
+      }
+    default:
+      return state;
+  }
+};
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../actions/cartActions";
 
 export default function CartScreen(props) {
+  // Query String la parameter
   const productId = props.match.params.id;
+  // Truyen query String trong qua trinh dieu huong
   const quantity = props.location.search
     ? Number(props.location.search.split("=")[1])
     : 1;
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, quantity));
+    }
+  }, [dispatch, productId, quantity]);
 
   return (
     <div>
