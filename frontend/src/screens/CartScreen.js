b1fb74229622b646279b1708bbe6cb6ae23fa4ed
TrinhDucLoc <westudymore@gmail.com>
2023-03-08 11:57:57 +0700

    Cart Page - Cart Screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { addToCart } from "../actions/cartActions";
+import MessageBox from "../components/MessageBox";
 
 export default function CartScreen(props) {
   // Query String la parameter
@@ -10,6 +12,9 @@ export default function CartScreen(props) {
     ? Number(props.location.search.split("=")[1])
     : 1;
 
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
   const dispatch = useDispatch();
   useEffect(() => {
     if (productId) {
@@ -17,15 +22,99 @@ export default function CartScreen(props) {
     }
   }, [dispatch, productId, quantity]);
 
+  const removeFromCartHandler = (productId) => {
+    //
+  };
+  const checkoutHandler = () => {
+    props.history.push("/signin?redirect=shipping");
+  };
+
   return (
-    <div>
-      <div className="row center">
-        <div>
-          <h1>Hi</h1>
-          <p>
-            Cart have product with productId: {productId} with quantity:{" "}
-            {quantity}
-          </p>
+    <div className="row top">
+      <div className="col-2">
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <MessageBox>
+            Cart is empty. <Link to="/">Go shopping</Link>
+          </MessageBox>
+        ) : (
+          <ul>
+            {cartItems.map((item) => (
+              <li key={item.product}>
+                <div className="row">
+                  {/* image */}
+                  <div>
+                    <img
+                      src={item.image}
+                      alt={item.name}
+                      className="small"
+                    ></img>
+                  </div>
+                  {/* name */}
+                  <div className="min-30">
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </div>
+                  {/* product cost */}
+                  <div>${item.price}</div>
+                  {/* quantity */}
+                  <div>
+                    <select
+                      value={item.quantity}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  {/* Sub Product Cost */}
+                  {/* <div>${item.price * item.quantity}</div> */}
+
+                  {/* Delete Button */}
+                  <div>
+                    <button
+                      type="button"
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <div className="col-1">
+        <div className="card card-body">
+          <ul>
+            <li>
+              <h2>
+                Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)} items)
+                : ${cartItems.reduce((a, c) => a + c.price * c.quantity, 0)}
+              </h2>
+              {/* <h2>
+                Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)} items) : $
+                {cartItems.reduce((a, c) => a + c.price * c.quantity, 0)}
+              </h2> */}
+            </li>
+            <li>
+              <button
+                type="button"
+                onClick={checkoutHandler}
+                className="primary block"
+                disabled={cartItems.length === 0}
+              >
+                Go to checkout
+              </button>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
