ce2b4a39f6778695309b8965613a4e2645a48cf4
TrinhDucLoc <westudymore@gmail.com>
2023-03-10 10:07:01 +0700

    Signin Page - SignIn screen and action

--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -1,12 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
-export default function SigninScreen() {
+import { signin } from "../actions/userActions";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+
+export default function SigninScreen(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  //
+  const redirect = props.location.search
+    ? props.location.search.split("=")[1]
+    : "/";
+
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo, loading, error } = userSignin;
+
+  const dispatch = useDispatch();
+  //
+
   const submitHandler = (e) => {
     e.preventDefault();
+    // TODO: sign in action
+    dispatch(signin(email, password));
   };
+
+  useEffect(() => {
+    if (userInfo) {
+      props.history.push(redirect);
+    }
+  }, [props.history, redirect, userInfo]);
 
   return (
     <div>
@@ -15,6 +40,8 @@ export default function SigninScreen() {
         <div>
           <h1>Sign In</h1>
         </div>
+        {loading && <LoadingBox></LoadingBox>}
+        {error && <MessageBox variant="danger">{error}</MessageBox>}
 
         {/* Input: username, label value and onChange */}
         <div>
