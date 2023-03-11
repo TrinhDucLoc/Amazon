9aafee8d4f59c4551edcbb2040b3a22bf05ef081
TrinhDucLoc <westudymore@gmail.com>
2023-03-12 02:45:21 +0700

    28. Order API

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import orderRouter from "./routers/orderRouter.js";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
 
@@ -12,6 +13,7 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
